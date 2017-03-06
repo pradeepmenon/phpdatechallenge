edc81ef1a17d9b774a821a6a9bbf465eb5f4d5b4
John Field <John.Field@gMail.com>
2017-03-06 19:37:05 +0000

    Basic date setting / validation, including leap years.

--- src/MyDate.php
+++ src/MyDate.php
@@ -1,18 +1,148 @@
 <?php
 
-  class MyDate {
+/**
+ * Clean-room implementation of Date-like functionality.
+ * 
+ * The phrase "for our purposes" appears a lot, because we're
+ * implementing only the functionality that meets the tests - 
+ * see README.md for more info.
+ * 
+ * @author John Field <John.Field@gMail.com>
+ */
+class MyDate
+{
 
-    public static function diff($start, $end) {
+    /**
+     * The date's year - for our purposes, an integer.
+     *
+     * @var int
+     */
+    protected $year;
 
-      // Sample object:
-      return (object)array(
-        'years' => null,
-        'months' => null,
-        'days' => null,
-        'total_days' => null,
-        'invert' => null
-      );
+    /**
+     * The date's month - for our purposes, a positive integer.
+     *
+     * @var int
+     */
+    protected $month;
 
+    /**
+     * The date's day - for our purposes, a positive integer.
+     *
+     * @var int
+     */
+    protected $day;
+
+    /**
+     * Constructor.
+     *
+     * @param string $date            
+     */
+    public function __construct($date)
+    {
+        $this->set($date);
     }
 
-  }
+    /**
+     * Setter.
+     *
+     * For our purposes, we only accept “YYYY/MM/DD”, performing basic validation.
+     *
+     * @param string $date            
+     * @throws \Exception
+     */
+    public function set($date)
+    {
+        $dateSplit = explode('/', $date);
+        if (count($dateSplit) != 3) {
+            throw new \Exception("Date '$date' was invalid.");
+        }
+        // Validate ranges.
+        // A valid value may well be a string (e.g. "03"), so convert, then validate, then pad out.
+        
+        $year = (int) $dateSplit[0];
+        $year = filter_var($year, FILTER_VALIDATE_INT);
+        if (! $year) {
+            throw new \Exception("Year '{$dateSplit[0]}' was invalid.");
+        }
+        $year = str_pad($year, 4, '0', STR_PAD_LEFT);
+        
+        $month = (int) $dateSplit[1];
+        $month = filter_var($month, FILTER_VALIDATE_INT, array(
+            "options" => array(
+                "min_range" => 1,
+                "max_range" => 12
+            )
+        ));
+        
+        if (! $month) {
+            throw new \Exception("Month '{$dateSplit[1]}' was invalid.");
+        }
+        $month = str_pad($month, 2, '0', STR_PAD_LEFT);
+        $day = (int) $dateSplit[2];
+        $day = filter_var($day, FILTER_VALIDATE_INT, array(
+            "options" => array(
+                "min_range" => 1,
+                "max_range" => 31
+            )
+        ));
+        
+        if (! $day) {
+            throw new \Exception("Day '{$dateSplit[2]}' was invalid.");
+        }
+        $day = str_pad($day, 2, '0', STR_PAD_LEFT);
+        
+        // Finally, validate the days in the month.
+        // https://en.wikipedia.org/wiki/Thirty_Days_Hath_September
+        // Going by the spec, we don't need to know month names.
+        switch ((int) $month) {
+            case 9:
+            case 4:
+            case 6:
+            case 11:
+                $maxDaysinMonth = 30;
+                break;
+            case 2:
+                $maxDaysinMonth = 28;
+                if (($year % 4) == 0) {
+                    // Leap year.
+                    $maxDaysinMonth = 29;
+                }
+                break;
+            default:
+                $maxDaysinMonth = 31;
+        }
+        if ((int) $day > $maxDaysinMonth) {
+            throw new \Exception("Day '$day' was invalid for month '$month'.");
+        }
+        
+        // Hooray!
+        $this->year = $year;
+        $this->month = $month;
+        $this->date = $day;
+    }
+
+    /**
+     * Return difference between two dates.
+     *
+     * For our purposes, the returned object is similar to
+     * http://php.net/manual/en/class.dateinterval.php
+     *
+     * @param string $start            
+     * @param string $end            
+     * @return StdClass
+     */
+    public static function diff($start, $end)
+    {
+        // Init our diff object.
+        $return = (object) array(
+            'years' => null,
+            'months' => null,
+            'days' => null,
+            'total_days' => null,
+            'invert' => null
+        );
+        
+        return $return;
+    }
+}
--- tests/MyDateTest.php
+++ tests/MyDateTest.php
@@ -1,184 +1,263 @@
 <?php
 
-  class MyDateTest extends PHPUnit_Framework_TestCase {
-    
-    public function testSimpleTotalDays() {
+class MyDateTest extends PHPUnit_Framework_TestCase
+{
 
-      $this->assertTotalDays('2014/01/01', '2014/01/04');
-
+    public function providerValidDates()
+    {
+        return array(
+            array(
+                '01/01/01'
+            ),
+            array(
+                '2017/03/06'
+            ),
+            array(
+                '2016/02/29'
+            )
+        ) // A valid leap day
+;
     }
 
-    public function testSimpleDays() {
-
-      $this->assertDays('2014/01/01', '2014/01/04');
-      
-    }
-    
-    public function testSimpleMonths() {
-
-      $this->assertMonths('2014/01/01', '2014/03/01');
-      
-    }
-    
-    public function testSimpleYears() {
-
-      $this->assertYears('2014/01/01', '2015/01/01');
-      
+    /**
+     * Verify basic date validation.
+     *
+     * @dataProvider providerValidDates
+     * @group validate
+     */
+    public function testValidDates($date)
+    {
+        $date = new MyDate($date);
     }
 
-    public function testInvertDayTrue() {
-
-      $this->assertInvert('2015/01/02', '2015/01/01');
-      
+    public function providerInvalidDates()
+    {
+        return array(
+            array(
+                'foo'
+            ),
+            array(
+                '2017/01/32'
+            ), // Not a valid day
+            array(
+                '2017/13/01'
+            ), // Not a valid month
+            array(
+                '2017/09/31'
+            ), // Not a valid day of the month
+            array(
+                '2017/02/29'
+            )
+        ) // Not a valid leap day
+;
     }
 
-    public function testInvertMonthTrue() {
-
-      $this->assertInvert('2015/02/02', '2015/01/01');
-      
+    /**
+     * @dataProvider providerInvalidDates
+     * @group validate
+     * @expectedException Exception
+     */
+    public function testInvalidDates($date)
+    {
+        $date = new MyDate($date);
     }
 
-    public function testInvertYearTrue() {
-
-      $this->assertInvert('2016/01/01', '2015/01/01');
-      
+    public function testSimpleTotalDays()
+    {
+        $this->assertTotalDays('2014/01/01', '2014/01/04');
     }
 
-    public function testInvertDayFalse() {
-
-      $this->assertInvert('2015/01/01', '2015/01/02');
-      
+    public function testSimpleDays()
+    {
+        $this->assertDays('2014/01/01', '2014/01/04');
     }
 
-    public function testInvertMonthFalse() {
-
-      $this->assertInvert('2015/01/01', '2015/02/01');
-      
+    public function testSimpleMonths()
+    {
+        $this->assertMonths('2014/01/01', '2014/03/01');
     }
 
-    public function testInvertYearFalse() {
-
-      $this->assertInvert('2015/01/01', '2016/01/01');
-      
-    }
-    
-    public function testComplexTotalDays() {
-
-      $this->assertTotalDays('2013/01/01', '2015/05/15');
-
+    public function testSimpleYears()
+    {
+        $this->assertYears('2014/01/01', '2015/01/01');
     }
 
-    public function testComplexDays() {
-
-      $this->assertDays('2013/03/21', '2015/07/31');
-      
-    }
-    
-    public function testComplexMonths() {
-
-      $this->assertMonths('2013/06/15', '2015/03/01');
-      
-    }
-    
-    public function testComplexYears() {
-
-      $this->assertYears('2013/09/13', '2015/07/01');
-      
-    }
-    
-    public function testLeapYearTotalDays() {
-
-      $this->assertTotalDays('2013/01/01', '2017/05/15');
-
+    public function testInvertDayTrue()
+    {
+        $this->assertInvert('2015/01/02', '2015/01/01');
     }
 
-    public function testLeapYearDays() {
-
-      $this->assertDays('2013/03/21', '2017/07/31');
-      
-    }
-    
-    public function testLeapYearMonths() {
-
-      $this->assertMonths('2013/06/15', '2017/03/01');
-      
-    }
-    
-    public function testLeapYearYears() {
-
-      $this->assertYears('2013/09/13', '2017/07/01');
-      
-    }
-    
-    public function testInvertLeapYearTrue() {
-
-      $this->assertInvert('2017/03/16', '2013/06/18');
-      
-    }
-    
-    public function testMultipleLeapYearTotalDays() {
-
-      $this->assertTotalDays('2013/01/01', '2029/05/15');
-
+    public function testInvertMonthTrue()
+    {
+        $this->assertInvert('2015/02/02', '2015/01/01');
     }
 
-    public function testMultipleLeapYearDays() {
-
-      $this->assertDays('2013/03/21', '2028/07/31');
-      
-    }
-    
-    public function testMultipleLeapYearMonths() {
-
-      $this->assertMonths('2013/06/15', '2029/03/01');
-      
-    }
-    
-    public function testMultipleLeapYearYears() {
-
-      $this->assertYears('2013/09/13', '2029/07/01');
-      
-    }
-    
-    public function testInvertMultipleLeapYearTrue() {
-
-      $this->assertInvert('2029/03/16', '2013/06/18');
-      
+    public function testInvertYearTrue()
+    {
+        $this->assertInvert('2016/01/01', '2015/01/01');
     }
 
-    private function assertYears($s, $e) {
-      $d = MyDate::diff($s, $e);
-      $a = $this->dateDiff($s, $e);
-      $this->assertSame($a->y, $d->years);
+    public function testInvertDayFalse()
+    {
+        $this->assertInvert('2015/01/01', '2015/01/02');
     }
 
-    private function assertMonths($s, $e) {
-      $d = MyDate::diff($s, $e);
-      $a = $this->dateDiff($s, $e);
-      $this->assertSame($a->m, $d->months);
+    public function testInvertMonthFalse()
+    {
+        $this->assertInvert('2015/01/01', '2015/02/01');
     }
 
-    private function assertDays($s, $e) {
-      $d = MyDate::diff($s, $e);
-      $a = $this->dateDiff($s, $e);
-      $this->assertSame($a->d, $d->days);
+    public function testInvertYearFalse()
+    {
+        $this->assertInvert('2015/01/01', '2016/01/01');
     }
 
-    private function assertTotalDays($s, $e) {
-      $d = MyDate::diff($s, $e);
-      $a = $this->dateDiff($s, $e);
-      $this->assertSame($a->days, $d->total_days);
+    public function testComplexTotalDays()
+    {
+        $this->assertTotalDays('2013/01/01', '2015/05/15');
     }
 
-    private function assertInvert($s, $e) {
-      $d = MyDate::diff($s, $e);
-      $a = $this->dateDiff($s, $e);
-      $this->assertSame((bool)$a->invert, $d->invert);
+    public function testComplexDays()
+    {
+        $this->assertDays('2013/03/21', '2015/07/31');
     }
 
-    private function dateDiff($start, $end) {
-      $start = DateTime::createFromFormat('Y/m/d', $start);
-      $end = DateTime::createFromFormat('Y/m/d', $end);
-      return $start->diff($end);
+    public function testComplexMonths()
+    {
+        $this->assertMonths('2013/06/15', '2015/03/01');
     }
-  }
+
+    public function testComplexYears()
+    {
+        $this->assertYears('2013/09/13', '2015/07/01');
+    }
+
+    public function testLeapYearTotalDays()
+    {
+        $this->assertTotalDays('2013/01/01', '2017/05/15');
+    }
+
+    public function testLeapYearDays()
+    {
+        $this->assertDays('2013/03/21', '2017/07/31');
+    }
+
+    public function testLeapYearMonths()
+    {
+        $this->assertMonths('2013/06/15', '2017/03/01');
+    }
+
+    public function testLeapYearYears()
+    {
+        $this->assertYears('2013/09/13', '2017/07/01');
+    }
+
+    public function testInvertLeapYearTrue()
+    {
+        $this->assertInvert('2017/03/16', '2013/06/18');
+    }
+
+    public function testMultipleLeapYearTotalDays()
+    {
+        $this->assertTotalDays('2013/01/01', '2029/05/15');
+    }
+
+    public function testMultipleLeapYearDays()
+    {
+        $this->assertDays('2013/03/21', '2028/07/31');
+    }
+
+    public function testMultipleLeapYearMonths()
+    {
+        $this->assertMonths('2013/06/15', '2029/03/01');
+    }
+
+    public function testMultipleLeapYearYears()
+    {
+        $this->assertYears('2013/09/13', '2029/07/01');
+    }
+
+    public function testInvertMultipleLeapYearTrue()
+    {
+        $this->assertInvert('2029/03/16', '2013/06/18');
+    }
+
+    /**
+     * Verify MyDate's behaviour against inbuilt PHP.
+     *
+     * @param string $s            
+     * @param string $e            
+     */
+    private function assertYears($s, $e)
+    {
+        $d = MyDate::diff($s, $e);
+        $a = $this->dateDiff($s, $e);
+        $this->assertSame($a->y, $d->years);
+    }
+
+    /**
+     * Verify MyDate's behaviour against inbuilt PHP.
+     *
+     * @param string $s            
+     * @param string $e            
+     */
+    private function assertMonths($s, $e)
+    {
+        $d = MyDate::diff($s, $e);
+        $a = $this->dateDiff($s, $e);
+        $this->assertSame($a->m, $d->months);
+    }
+
+    /**
+     * Verify MyDate's behaviour against inbuilt PHP.
+     *
+     * @param string $s            
+     * @param string $e            
+     */
+    private function assertDays($s, $e)
+    {
+        $d = MyDate::diff($s, $e);
+        $a = $this->dateDiff($s, $e);
+        $this->assertSame($a->d, $d->days);
+    }
+
+    /**
+     * Verify MyDate's behaviour against inbuilt PHP.
+     *
+     * @param string $s            
+     * @param string $e            
+     */
+    private function assertTotalDays($s, $e)
+    {
+        $d = MyDate::diff($s, $e);
+        $a = $this->dateDiff($s, $e);
+        $this->assertSame($a->days, $d->total_days);
+    }
+
+    /**
+     * Verify MyDate's behaviour against inbuilt PHP.
+     *
+     * @param string $s            
+     * @param string $e            
+     */
+    private function assertInvert($s, $e)
+    {
+        $d = MyDate::diff($s, $e);
+        $a = $this->dateDiff($s, $e);
+        $this->assertSame((bool) $a->invert, $d->invert);
+    }
+
+    /**
+     * Helper method that uses inbuilt PHP for verification.
+     *
+     * @param string $start            
+     * @param string $end            
+     * @return \DateInterval
+     */
+    private function dateDiff($start, $end)
+    {
+        $start = DateTime::createFromFormat('Y/m/d', $start);
+        $end = DateTime::createFromFormat('Y/m/d', $end);
+        return $start->diff($end);
+    }
+}
